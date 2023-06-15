e1dbda87bab5828aef84be18bd1d99f26a2c691a
Davi Marques <davi.amqs@hotmail.com>
2023-06-15 12:22:28 -0300

    add migration on relational table Specification Car

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController";
-import { ListAvailableCarsController } from "../../../../modules/cars/useCases/listAvailableCars/listAvailableCarsController";
+import { ListAvailableCarsController } from "../../../../modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
